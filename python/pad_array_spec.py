128fceae3441cba5aab24ef1dcb97a57fddf1a6d
awhitford10 <awhitford@Andrews-Mac-mini.local>
2021-06-01 15:19:43 -0400

    Andrew_pad array_solution

--- python/pad_array_spec.py
+++ python/pad_array_spec.py
@@ -1,1 +1,18 @@
 # Write unit tests!
+import unittest
+from pad_array import pad
+
+class PadTest(unittest.TestCase):
+
+    def test_type(self):
+        self.assertEqual(type(pad([1,2,3],5)),list)
+
+    def test_1(self):
+        self.assertEqual(pad([1,2,3],5), [1,2,3,None,None])
+
+    def test_2(self):
+        self.assertEqual(pad([1,2,3],5,'apple'), [1,2,3,'apple','apple'])
+
+
+if __name__ == '__main__':
+    unittest.main()
