a7ad32573c163484a5981d815ac57fc8bdd30eb9
Shawn Kiernan <kiernan.shawn@gmail.com>
2021-06-01 10:56:26 -0500

    Shawn's python solution to pad array with unittest

--- python/pad_array.py
+++ python/pad_array.py
@@ -1,2 +1,12 @@
-#REMEMBER TO PSEUDOCODE
+#Compare length of ARRAY to MIN_SIZE
+#If length ARRAY is less than MIN_SIZE append VALUE to end of
+#ARRAY equal to MIN_SIZE minus length of ARRAY
 def pad(array, min_size, value = None):
+    if (len(array) < min_size):
+        i = 0
+        number_elements_to_add = min_size - len(array)
+        while i < number_elements_to_add:
+            array.append(value)
+            i += 1
+    
+    return array
--- python/pad_array_spec.py
+++ python/pad_array_spec.py
@@ -1,1 +1,24 @@
 # Write unit tests!
+from pad_array import pad
+import unittest # imports the Unit Test library
+
+class PadTestCases(unittest.TestCase):
+
+    def test_pad_1(self):
+        output = pad([1,2,3], 5, 'apple')
+        self.assertListEqual(output, [1,2,3,'apple', 'apple'])
+
+    def test_pad_2(self):
+        output = pad([1,2,3,4,5], 4)
+        self.assertListEqual(output, [1,2,3,4,5])
+
+    def test_pad_3(self):
+        output = pad([], 4, '1')
+        self.assertListEqual(output, ['1', '1', '1', '1'])
+
+    def test_pad_4(self):
+        output = pad([], 4)
+        self.assertListEqual(output, [None, None, None, None])
+
+if __name__ == '__main__':
+    unittest.main()
