f6836ceba8686f5454ce72165587499171452c78
RenoAverill <reno_averill@yahoo.com>
2021-06-01 11:58:30 -0600

    Reno's solution pad_array with Unittest

--- python/pad_array_spec.py
+++ python/pad_array_spec.py
@@ -1,1 +1,20 @@
-# Write unit tests!
+from pad_array import pad
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_output(self):
+        output = pad([1,2,3,4],5, value = 'extra num')
+        self.assertEqual(output, [1,2,3,4, 'extra num'])
+
+    def test_list(self):
+        output = type(pad([1,2,3,4],5, value = 'extra num'))       
+        self.assertEqual(output, list)
+        
+
+    def test_len(self):
+        output = len(pad([1,2,3,4],5, value = 'extra num'))
+        self.assertEqual(output, 5)
+
+if __name__ == '__main__':
+    unittest.main()
